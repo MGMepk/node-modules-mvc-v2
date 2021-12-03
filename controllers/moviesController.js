af260fe3054ff42ea6104ac9dca4ac039de521a7
Manuel García <manuelgarcia.mgm@gmail.com>
2021-12-03 18:55:15 +0100

    ex1

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,31 +1,39 @@
 import moviesModel from '../models/moviesModel.js';
 
 const getAllMovies = (data_movie) => {
-   
+  data_movie.res = moviesModel.getMovies();
 }
 
 const getMovieById = (data_movie) => {
-    
+  data_movie.res.push(moviesModel.getMovieById(data_movie.req.id));
+  console.log(typeof data_movie.res);
 }
 
 const removeMovie = (data_movie) => {
-  
+  moviesModel.removeMovie(data_movie.req.id);
+  data_movie.res = moviesModel.getMovies();
 }
 
 const createMovie = (data_movie) => {
-
-   
+    moviesModel.createMovie(data_movie.req);
+    data_movie.res = moviesModel.getMovies();
 }
 
 const updateMovie = (data_movie) => {
-  
+  moviesModel.updateMovie(data_movie.req);
+  data_movie.res = moviesModel.getMovies();
 
 }
 
 const getMovieBy = (data_movie) => {
-   
+   data_movie.res = moviesModel.getMovieBy(data_movie.req);
 }
 
 export default {
-   
+   getAllMovies,
+   getMovieById,
+   removeMovie,
+   createMovie,
+   updateMovie,
+   getMovieBy
 }
